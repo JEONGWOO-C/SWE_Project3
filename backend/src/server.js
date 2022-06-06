0d04496aa3b960ff11b41ead77f388b2e1241539
JEONGWOO-C <dnsgmlehdtod@gmail.com>
2022-06-06 17:58:06 +0900

    FEAT : Login function update (Encrypted user ID & Password)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { init } from './config/db.js';
 import login from './api/User/login.js';
+import cors from 'cors';
 
 const connection = init();
-
 const app = express();
 
+let corsOption = {
+  origin: 'http://localhost:3000', // 허락하는 요청 주소
+  credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+app.use(cors(corsOption)); // CORS 미들웨어 추가
 app.use(
   express.json({
     limit: '1000mb',
