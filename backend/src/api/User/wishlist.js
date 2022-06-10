77d0175dd8ea68be430b71ab7187e299b69d2d8e
JEONGWOO-C <dnsgmlehdtod@gmail.com>
2022-06-10 22:33:32 +0900

    Image Source URL Change #3

--- backend/src/api/User/wishlist.js
+++ backend/src/api/User/wishlist.js
@@ -4,11 +4,15 @@ export default async (app, connection) => {
   app.use("/wishlist", async (req, res, next) => {
     const { id } = req.query;
     await connection.query(
-      "SELECT PO.postnum, PO.title, PO.postDate, PR.price, PH.imgnum, PH.photo FROM favorite F, posts PO, product PR, photos PH WHERE id = 'test2' AND F.postnum = PO.postnum AND F.postnum = PR.postnum AND F.postnum = PH.postnum ORDER BY PO.postDate desc;",
+      "SELECT PO.postnum, PO.title, PR.isSelling, PO.views, PO.fav, PO.postDate, PR.price, PH.photo FROM favorite F, posts PO, product PR, photos PH WHERE id = 'test2' AND F.postnum = PO.postnum AND F.postnum = PR.postnum AND F.postnum = PH.postnum ORDER BY PO.postDate desc;",
       [id],
       (error, data) => {
         if (error) console.log(error);
         const result = data;
+        for (var i = 0; i < result.length; i++) {
+          result[i].photo =
+            req.protocol + "://" + req.get("host") + result[i].photo;
+        }
         console.log(result);
         return res.send(result);
       }
