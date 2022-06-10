57960e65c58d55d50a4c9e8dd4e809207afa6b8e
izar111 <ghktks98@gmail.com>
2022-06-10 23:14:24 +0900

    Recent View Post page Content add & Upload Image file name change number

--- frontend/src/pages/Viewed.js
+++ frontend/src/pages/Viewed.js
@@ -13,7 +13,9 @@ import {
 } from '../components/Card';
 import styled from 'styled-components';
 import logo from '../imgs/logo192.png'; // 예시 사진
-import { ProductWrapper, Product, Title } from '../components/Product'
+import { ProductWrapper, Product, Title } from '../components/Product';
+import { getInfoFromCookie } from '../components/Auth';
+import axios from "axios";
 
 const Body = styled.div`
   display: flex;
@@ -24,13 +26,50 @@ const Body = styled.div`
 
 
 const Viewed = ({ history }) => {
+  var [recentViewTop5,setRecentViewTop5] = useState([]);
+  var [recentViewTop10,setRecentViewTop10] = useState([]);
+  var [recentView,setRecentView] = useState([]);
+  useEffect(() => {
+    console.log(getInfoFromCookie());
+    axios
+      .post("http://localhost:4000/userRecentPosts",{
+        id:'test1'
+      })
+      .then(({ data }) => setRecentView(data));
+  }, [])
+
+  console.log(recentView);
+
   return (
     <Body style={{}}>
       <CardWrapper>
         <Title>
           최근본상품
         </Title>
-        최근본상품 데이터 준비중
+        <ProductWrapper>
+          {recentView.map((item, ind) => {
+            if(ind<5)
+              return(
+                <Product
+                  key={item.title}
+                  img={item.photo}
+                  name={item.title}
+                  price={item.price + "원"}
+                />)
+          })}
+        </ProductWrapper>
+        <ProductWrapper>
+          {recentView.map((item, ind) => {
+            if(ind>=5)
+              return(
+                <Product
+                  key={item.title}
+                  img={item.photo}
+                  name={item.title}
+                  price={item.price + "원"}
+                />)
+          })}
+        </ProductWrapper>
       </CardWrapper>
 
     </Body >
