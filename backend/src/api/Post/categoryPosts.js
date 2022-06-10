2adfa18bd7571b0dad6b611fae0fab8426d11836
JEONGWOO-C <dnsgmlehdtod@gmail.com>
2022-06-10 14:13:02 +0900

    게시글 정보 추가

--- backend/src/api/Post/categoryPosts.js
+++ backend/src/api/Post/categoryPosts.js
@@ -4,7 +4,7 @@ export default (app, connection) => {
     console.log(req.query);
     console.log(category);
     connection.query(
-      "SELECT PO.postnum, PO.title, PO.postDate, PR.price, PH.photo, PR.category FROM posts PO, product PR, photos PH WHERE category = ? AND PO.postnum = PR.postnum AND PO.postnum = PH.postnum ORDER BY PO.postDate desc;",
+      "SELECT PO.postnum, PO.postDate, PO.title, PR.price, PR.isSelling, PO.views, PO.fav, PH.photo, PR.category FROM posts PO, product PR, photos PH WHERE category = ? AND PO.postnum = PR.postnum AND PO.postnum = PH.postnum ORDER BY PO.postDate desc;",
       [category],
       (error, data) => {
         if (error) console.log(error);
