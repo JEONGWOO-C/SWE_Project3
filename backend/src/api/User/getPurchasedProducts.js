424293d0c775ed38e2f64947fba1c2a8eee913f8
JEONGWOO-C <dnsgmlehdtod@gmail.com>
2022-06-13 21:35:02 +0900

    판매내역 구매내역 backend 데이터 연동완료

---
+++ backend/src/api/User/getPurchasedProducts.js
@@ -0,0 +1,17 @@
+import auth from "../modules/auth.js";
+export default async (app, connection) => {
+  app.get("/getPurchasedProducts", auth);
+  app.use("/getPurchasedProducts", async (req, res, next) => {
+    const { id } = req.query;
+    console.log(id);
+    await connection.query(
+      "SELECT PO.postnum, PO.title, PR.isSelling, PO.views, PO.fav, PO.postDate, PR.price, PH.photo FROM posts PO, product PR, photos PH WHERE PO.postnum = PR.postnum AND PO.postnum = PH.postnum AND PR.buyer_id = ? AND PR.isSelling = false;",
+      [id],
+      (error, data) => {
+        if (error) console.log(error);
+        const result = data;
+        return res.send(result);
+      }
+    );
+  });
+};
