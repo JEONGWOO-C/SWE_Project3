874136742109f76b37f2525a7af807f5d1cebaa8
JEONGWOO-C <dnsgmlehdtod@gmail.com>
2022-06-13 21:45:59 +0900

    비밀번호 변경 오류 수정

--- backend/src/api/User/userUpdate.js
+++ backend/src/api/User/userUpdate.js
@@ -2,15 +2,17 @@ import auth from "../modules/auth.js";
 export const profileUpdate = async (app, connection) => {
   app.post("/profileUpdate", auth, async (req, res, next) => {
     const { id } = req.query;
-    const { username,info } = req.body;
+    const { username, info } = req.body;
     console.log(id);
     await connection.query(
       "UPDATE users SET username = ?, info = ? WHERE id = ?;",
-      [username,info,id],
+      [username, info, id],
       (error, data) => {
         console.log(data);
-        if (error) {console.log(error);res.send({result:false,error:error});}
-        else res.send({result:true});
+        if (error) {
+          console.log(error);
+          res.send({ result: false, error: error });
+        } else res.send({ result: true });
       }
     );
   });
@@ -18,46 +20,48 @@ export const profileUpdate = async (app, connection) => {
 export const privateUpdate = async (app, connection) => {
   app.post("/privateUpdate", auth, async (req, res, next) => {
     const { id } = req.query;
-    const { email, phone, age } = req.body
+    const { email, phone, age } = req.body;
     await connection.query(
       "UPDATE users SET email = ?, phone = ?, age = ? WHERE id = ?;",
-      [email,phone,age,id],
+      [email, phone, age, id],
       (error, data) => {
         console.log(data);
-        if (error) {console.log(error);res.send({result:false,error:error});}
-        else res.send({result:true});
+        if (error) {
+          console.log(error);
+          res.send({ result: false, error: error });
+        } else res.send({ result: true });
       }
     );
   });
 };
 
 export const passwordUpdate = async (app, connection) => {
-    app.post("/passwordUpdate", auth, async (req, res, next) => {
-        const { id } = req.query;
-        const { origin_pw, new_pw, confirm_pw } = req.body
-        if(new_pw !== confirm_pw)
-        res.send({result:false,error:'새 비밀번호 불일치'})
-      
-        await connection.query(
-            "SELECT pw FROM users WHERE id = ?;",
-            [id],
+  app.post("/passwordUpdate", auth, async (req, res, next) => {
+    const { id } = req.query;
+    const { origin_pw, new_pw, confirm_pw } = req.body;
+
+    await connection.query(
+      "SELECT pw FROM users WHERE id = ?;",
+      [id],
+      (error, data) => {
+        console.log(data);
+        if (error) {
+          console.log(error);
+          res.send({ result: false, error: "error" });
+        } else if (origin_pw == data[0].pw) {
+          connection.query(
+            "UPDATE users SET pw = ? WHERE id = ?;",
+            [new_pw, id],
             (error, data) => {
-                console.log(data);
-                if (error) {console.log(error);res.send({result:false,error:error});}
-                else if(origin_pw == data[0].pw){
-                    connection.query(
-                        "UPDATE users SET pw = ? WHERE id = ?;",
-                        [new_pw,id],
-                        (error, data) => {
-                        console.log(data);
-                        if (error) {console.log(error);res.send({result:false,error:error});}
-                        else res.send({result:true});
-                        }
-                    );
-                }else
-                 res.send({result:false,error:'비밀번호 불일치'});
+              console.log(data);
+              if (error) {
+                console.log(error);
+                res.send({ result: false, error: error });
+              } else res.send({ result: true });
             }
-        );
-        
-    });
-}
+          );
+        } else res.send({ result: false, error: "비밀번호 불일치" });
+      }
+    );
+  });
+};
