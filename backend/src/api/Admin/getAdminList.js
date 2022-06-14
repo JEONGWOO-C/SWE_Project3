c08561348f2a46d68bfe65c6a2573e73c2b93183
lllminsuk <umsun3@naver.com>
2022-06-15 06:11:47 +0900

    회원관리 & 관리자관리 페이지 수정

--- backend/src/api/Admin/getAdminList.js
+++ backend/src/api/Admin/getAdminList.js
@@ -6,14 +6,15 @@ export default async (app, connection) => {
       (error, data) => {
         if (error) console.log(error);
         var result = []
-        for(var i=0;i<data.length;i++){
-           result.push({
-             id : data[i].id,
-             name : data[i].name,
-             phoe : data[i].phone,
-             email : data[i].email,
-             confirm : data[i].isApproved
-           });
+        for (var i = 0; i < data.length; i++) {
+          result.push({
+            adminname: data[i].adminname,
+            id: data[i].id,
+            name: data[i].name,
+            phone : data[i].phone,
+            email: data[i].email,
+            confirm: data[i].isApproved
+          });
         }
         return res.send(result);
       }
