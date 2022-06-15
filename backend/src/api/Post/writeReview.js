04af50d60dfea610b6dc29cc8ce5c83e0bfddc2f
JEONGWOO-C <dnsgmlehdtod@gmail.com>
2022-06-15 17:30:19 +0900

    후기 작성 res수정 및 게시물 수정 쿼리 작성완료 / 유저 평점 기본 값 3점으로 변경

--- backend/src/api/Post/writeReview.js
+++ backend/src/api/Post/writeReview.js
@@ -2,22 +2,28 @@ export default async (app, connection) => {
   app.post("/writeReview", async (req, res, next) => {
     const { postnum, score, buyer_id, review } = req.body;
 
+    if (review === "")
+      return res.send({ result: false, msg: "후기를 작성해 주세요." });
+
     connection.query(
-      "UPDATE users SET score = (score + ?)/2 WHERE id = ?",
-      [score, buyer_id],
+      "UPDATE users AS U, product AS P SET U.score = (score + ?)/2 AND P.review = ? WHERE U.id = ? AND P.postnum = ?",
+      [score, review, buyer_id, postnum],
       (error, data) => {
         if (error) {
           console.log(error);
-          res.send({ result: false });
+          res.send({ result: false, msg: "QUERY ERROR" });
         } else {
           connection.query(
             "UPDATE product SET review = ? WHERE postnum = ?;",
             [review, postnum],
             (error) => {
-              if (error) console.log(error);
+              if (error) res.send({ result: false, msg: "QUERY ERROR" });
             }
           );
-          res.send({ result: true });
+          res.send({
+            result: true,
+            msg: "후기 작성 및 판매자 평점 업데이트가 완료되었습니다.",
+          });
         }
       }
     );
